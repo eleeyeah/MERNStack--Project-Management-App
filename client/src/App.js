8a8e3b5ebb5c807be84af801c14c4791127b9a68
eleeyeah <ilietmks@gmail.com>
2022-10-27 00:46:34 +0200

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    <div className="container">
+      <h1>Ready to rumble</h1>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
