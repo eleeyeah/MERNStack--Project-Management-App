df667e92dad6f51bd79d97404c0fa43150f5901f
eleeyeah <ilietmks@gmail.com>
2022-11-02 23:42:08 +0100

    almost done

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,18 +2,19 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    //refetchQueries: [{ query: GET_CLIENTS }],
-    update(cache, { data: { deleteClient } }) {
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    /* update(cache, { data: { deleteClient } }) {
       const { clients } = cache.readQuery({ query: GET_CLIENTS });
       cache.writeQuery({
         query: GET_CLIENTS,
         data: { clients: clients.filter((c) => c.id !== deleteClient.id) },
       });
-    },
+    }, */
   });
 
   return (
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 
@@ -26,6 +28,10 @@ export default function Project() {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
