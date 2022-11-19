e1bb2cb46bd20f586196653c850c0d6ff35345fe
eleeyeah <ilietmks@gmail.com>
2022-11-19 19:11:56 +0100

    nologo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
       <div className="container">
         <a className="navbar-brand" href="/">
           <div className="d-flex">
-            <img src={logo} alt="logo" className="mr-2" />
+            {/* <img src={logo} alt="logo" className="mr-2" /> */}
             <div>Project Management</div>
           </div>
         </a>
